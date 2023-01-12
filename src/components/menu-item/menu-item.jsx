13e45530bdf57b18bd5a0be2dd8d971218c022da
husanbek123 <husanbek555@gmail.com>
2023-01-12 15:43:37 +0500

    3-commit

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './menu-item.scss'
+
+
+export default function Menu_item(props) {
+  return (
+    <div className="menu-item">
+        <div className="content">
+            <h1 className="title">{props.title}</h1>
+            <span className="subtitle">{props.subtitle}</span>
+        </div>
+    </div>
+  )
+}
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -5,7 +5,7 @@ const HomePage = () => {
     
     return (
         <div className="homepage">
-            <div className="directory-menu">
+            <div className="items-menu">
                 <div className="menu-item">
                     <div className="content">
                         <h1 className="title">HATS</h1>
